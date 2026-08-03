adf7bd4641ef8e8f5cc897c48c81c8fe0d25606e
christian dione <christian.dione@itd-hub.com>
2026-08-03 10:28:46 +0000

    ScrollToTop

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -52,7 +52,7 @@ export function Footer() {
                         <Link to="/offres">Offres d'emploi</Link>
                         <Link to="/formations">Formations</Link>
                         <Link to="/inscription?role=candidat">Créer mon profil</Link>
-                        <Link to="/inscription?role=candidat">Suivre mes candidatures</Link>
+                        <Link to="/connexion">Suivre mes candidatures</Link>
                     </div>
 
                     {/* RECRUTEURS */}
