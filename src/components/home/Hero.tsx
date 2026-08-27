028ce64f71176ac49337a6be89a837454ddf81f7
christian dione <christian.dione@itd-hub.com>
2026-08-27 14:54:34 +0000

    offre

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import talent1 from "../../assets/talent-44.png";
+import talent1 from "../../assets/talent-6.jpg";
 import { IconUsers, IconBriefcase, IconGlobe, IconShieldCheck } from "./icons";
 import { listerOffresPubliques } from "../../api/offreService";
 import "./Hero.css";
